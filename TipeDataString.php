9c1e533d4be2f7b4e9e47c4d06ef31e9a3875346
yasminramadhan <yasminramadhan638@gmail.com>
2024-07-17 11:01:21 +0700

    bagian 4

---
+++ TipeDataString.php
@@ -0,0 +1,33 @@
+<?php
+echo 'Name :';
+echo 'Nayla NUr Padilah';
+echo "\n";
+
+echo "Name : ";
+echo "Yasmin\t Nayla\t Ida\n";
+
+echo <<<EKO
+Selamat Belajar PHP
+Sekarang, Kita belajat tipe data string 
+ini adalah cara ke-3 membuat string
+bisa menggunakan heredoc
+EKO;
+
+echo <<<'EKO'
+Selamat Belajar PHP
+Sekarang, Kita belajat tipe data string 
+ini adalah cara ke-3 membuat string
+bisa menggunakan heredoc
+EKO;
+
+
+
+
+
+
+
+
+
+
+
+?>
